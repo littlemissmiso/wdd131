1be18945c0640131d26acdab162f1b562acc432c
littlemissmiso <todayisdelightful@gmail.com>
2025-01-10 22:32:38 -0700

    index (home page) project

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const date = new Date();
+let year = date.getFullYear();
+
+document.getElementById('year').textContent = year;
+
+
+let lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
