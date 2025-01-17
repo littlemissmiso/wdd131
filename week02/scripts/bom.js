c9915986c74351d18e72bfd1e4b925170860af69
littlemissmiso <todayisdelightful@gmail.com>
2025-01-17 11:41:27 -0700

    BOM html + js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,27 @@
-const input = document.querySelector('favoriteChapter');
+const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+//button event listener
+button.addEventListener("click", function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
 
-deleteButton.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+
+        deleteButton.addEventListener("click", function() {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+    else {
+        input.focus();
+    }
+});
