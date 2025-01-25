e2bfc08ee09332f06eb861eb6c7a696adf09dbd4
littlemissmiso <todayisdelightful@gmail.com>
2025-01-25 15:10:54 -0700

    Place edits

--- scripts/place.js
+++ scripts/place.js
@@ -1,6 +1,20 @@
+/* -----------Wind Chill Function------------*/
 
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature > 50 || windSpeed <= 3) {
+        return "N/A";
+    }
 
+    const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16))
+    return windChill.toFixed(1);
+    
+}
 
+let temperature = 28;
+let windSpeed = 6;
+
+let windChill = calculateWindChill(temperature, windSpeed);
+document.getElementById('windChill').textContent = windChill;
 
 
 /*------------Footer Settings----------------*/
