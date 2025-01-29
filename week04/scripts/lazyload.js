a94cdfe8936d7ea77453dd9b5754e18a4bcd7ecf
littlemissmiso <todayisdelightful@gmail.com>
2025-01-28 21:18:18 -0700

    Lazy Loading Practice

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,10 @@
+/*------------Footer Settings----------------*/
+
+const date = new Date();
+let year = date.getFullYear();
+
+document.getElementById('year').textContent = year;
+
+
+let lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
