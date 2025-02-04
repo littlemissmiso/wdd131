0ee7ddcf78b07793a4fd66aad28b3fe5d6cd5174
littlemissmiso <todayisdelightful@gmail.com>
2025-02-03 21:47:07 -0700

    Project Site Plan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,11 @@
+
+/*------------Footer Settings----------------*/
+
+const date = new Date();
+let year = date.getFullYear();
+
+document.getElementById('year').textContent = year;
+
+
+let lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
